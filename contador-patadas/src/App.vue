ad193a08f1e21f038bf363a1ec411ddd0c37b702
Henrique Silva <henriwasd@gmail.com>
2020-03-12 18:03:05 -0300

    Ajuste de nome do componente

--- contador-patadas/src/App.vue
+++ contador-patadas/src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <v-app>
     <v-content>
-      <HelloWorld />
+      <Cronometro />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import Cronometro from "./components/Cronometro";
 
 export default {
   name: "App",
 
   components: {
-    HelloWorld
+    Cronometro
   },
 
   data: () => ({
